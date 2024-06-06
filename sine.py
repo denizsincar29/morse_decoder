7f15bc5a486cf5d1434a736acd4eab495d6a77cd
deniz <deniz@r1oaz.ru>
2024-06-06 16:11:36 +0300

    illiminated threads.

--- sine.py
+++ sine.py
@@ -2,26 +2,20 @@
 
 import numpy as np
 import sounddevice as sd
-from queue import Queue
-from threading import Thread
 
 # returns a frame of a sine wave by index, or x position. e.g. x is an integer index of a frame, so x/sr is the time in seconds
 def sine_wave_frame(x, freq, sr, volume):
     return (volume*np.sin(2*np.pi*freq*x/sr)).astype(np.float32)
-class SineWavePlayer(Thread):
+class SineWavePlayer():
     def __init__(self):
-        super().__init__()
         self.stream=sd.OutputStream(channels=1, callback=self.sd_callback, dtype=np.float32)
-        self.queue=Queue()
         self.freq=440*2**(10/12)
         self.sr=44100
         self.volume=0.5
         # write frame by frame, so we need to keep track of the phase
         self.sine_phase=0
         self.running=False
-        self.daemon=True
         self.stream.start()
-        self.start()
 
     def sd_callback(self, outdata, frames, time, status):
         if self.running:
@@ -31,18 +25,18 @@ class SineWavePlayer(Thread):
         else:
             outdata[:]=0
 
-    def run(self):
-        while True:
-            if not self.queue.empty():
-                self.running=self.queue.get()
-                #self.volume=0.5*self.running  # we can multiply by boolean to get 0 or 0.5
-
     def play(self):
-        self.queue.put(True)
+        self.running=True
 
     def stop(self):
-        self.queue.put(False)
+        self.running=False
 
+    def close(self):
+        self.stream.stop()
+        self.stream.close()
+
+    def __del__(self):  # automatically close the stream when the object is deleted
+        self.close()
 
 if __name__=="__main__":
     from time import sleep
